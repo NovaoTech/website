5dbc20b399a89fedc8301925c4463ee469f8c7fc
Judah B <me@jtechnologies.xyz>
2022-04-11 11:20:12 -0500

    One last ditto, Prettier, etc.

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -13,64 +13,63 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
-	
 <script>
-	import Logo from './logo.svelte';
-	import '@fontsource/jetbrains-mono';
+  import Logo from "./logo.svelte";
+  import "@fontsource/jetbrains-mono";
 </script>
 
 <div id="header">
-	<div id="left">
-		<Logo type="light" color="#FFFFFF" size="50px" />
-	</div>
-	<div id="right">
-		<a href="/" class="navlink"><span>Home</span></a>
-		<span class="navlink">-</span>
-		<a href="/about" class="navlink"><span>About</span></a>
-		<span class="navlink">-</span>
-		<a href="/work" class="navlink"><span>Our Work</span></a>
-	</div>
+  <div id="left">
+    <Logo type="light" color="#FFFFFF" size="50px" />
+  </div>
+  <div id="right">
+    <a href="/" class="navlink"><span>Home</span></a>
+    <span class="navlink">-</span>
+    <a href="/about" class="navlink"><span>About</span></a>
+    <span class="navlink">-</span>
+    <a href="/work" class="navlink"><span>Our Work</span></a>
+  </div>
 </div>
 
 <style>
-	@media (prefers-color-scheme: dark) {
-		:root {
-			--nav-background-color: #2e2e2e;
-			--links-inactive-color: #b9b9b9;
-		}
-	}
-	@media (prefers-color-scheme: light) {
-		:root {
-			--nav-background-color: #ffffff;
-			--links-inactive-color: #2e2e2e;
-		}
-	}
-	#header {
-		background-color: var(--nav-background-color);
-		padding-top: 7.5px;
-		padding-bottom: 7.5px;
-		padding-right: 20px;
-		padding-left: 7.5px;
-		box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-		display: flex;
-		height: 50px;
-		justify-content: space-between;
-		align-items: center;
-	}
-	#left {
-		height: min-content;
-	}
-	#right {
-		height: min-content;
-	}
-	.navlink {
-		transition: color 0.5s;
-		font-family: 'JetBrains Mono';
-		color: var(--links-inactive-color);
-		text-decoration: none;
-		size: 10%;
-	}
-	a.navlink:hover {
-		color: rgba(24, 180, 177, 1);
-	}
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --nav-background-color: #2e2e2e;
+      --links-inactive-color: #b9b9b9;
+    }
+  }
+  @media (prefers-color-scheme: light) {
+    :root {
+      --nav-background-color: #ffffff;
+      --links-inactive-color: #2e2e2e;
+    }
+  }
+  #header {
+    background-color: var(--nav-background-color);
+    padding-top: 7.5px;
+    padding-bottom: 7.5px;
+    padding-right: 20px;
+    padding-left: 7.5px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
+    display: flex;
+    height: 50px;
+    justify-content: space-between;
+    align-items: center;
+  }
+  #left {
+    height: min-content;
+  }
+  #right {
+    height: min-content;
+  }
+  .navlink {
+    transition: color 0.5s;
+    font-family: "JetBrains Mono";
+    color: var(--links-inactive-color);
+    text-decoration: none;
+    size: 10%;
+  }
+  a.navlink:hover {
+    color: rgba(24, 180, 177, 1);
+  }
 </style>
--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -14,69 +14,86 @@
 	You should have received a copy of the GNU General Public License
 	along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 <script>
-	import Header from '../components/header.svelte';
-	import Footer from '../components/footer.svelte';
-	import Cover from '../components/cover.svelte';
+  import Header from "../components/header.svelte";
+  import Footer from "../components/footer.svelte";
+  import Cover from "../components/cover.svelte";
 </script>
+
 <svelte:head>
-	<title>Our Work | Novao</title>
-	<meta name="description" content="Novao Technologies is a FOSS company developing open products for the world.">
+  <title>Our Work | Novao</title>
+  <meta
+    name="description"
+    content="Novao Technologies is a FOSS company developing open products for the world."
+  />
 </svelte:head>
 <body>
-	<div id="header">
-		<Header />
-	</div>
-	{ #if true }
-	<Cover inner="<span>what we do.</span>" style="padding: 10px; color: rgba(0,0,0,.60);font-family: 'JetBrains Mono'; font-weight: 500; font-size: 12.9vh;"/>
-	{ :else }
-	<Cover inner="<span>we are novao.</span>" style="padding: 10px; color: rgba(0,0,0,.60);font-family: 'JetBrains Mono'; font-weight: 500; font-size: 14vh;"/>
-	{ /if }
-	<div id="content">
-		<h1>We're a <span class="highlight">startup.</span></h1>
-		<p class="endpara">This means that we're currently pursuing different projects to work on. Once we have a few ready, we'll update this page with their info :)</p>
-	</div>
-	
-	<Footer />
+  <div id="header">
+    <Header />
+  </div>
+  {#if true}
+    <Cover
+      inner="<span>what we do.</span>"
+      style="padding: 10px; color: rgba(0,0,0,.60);font-family: 'JetBrains Mono'; font-weight: 500; font-size: 12.9vh;"
+    />
+  {:else}
+    <Cover
+      inner="<span>we are novao.</span>"
+      style="padding: 10px; color: rgba(0,0,0,.60);font-family: 'JetBrains Mono'; font-weight: 500; font-size: 14vh;"
+    />
+  {/if}
+  <div id="content">
+    <h1>We're a <span class="highlight">startup.</span></h1>
+    <p class="endpara">
+      This means that we're currently pursuing different projects to work on.
+      Once we have a few ready, we'll update this page with their info :)
+    </p>
+  </div>
+
+  <Footer />
 </body>
 
 <style>
-	@media (prefers-color-scheme: dark) {
-		:root {
-			--text-color: #d0d0d0;
-		}
-	}
-	@media (prefers-color-scheme: light) {
-		:root {
-			--nav-background-color: #ffffff;
-			--text-color: #2e2e2e;
-		}
-	}
-	h1 {
-		font-family: 'JetBrains Mono';
-		padding-top: 15px;
-		padding-left: 10px;
-		color: var(--text-color);
-		font-size: 7vh;
-	}
-	p {
-		padding-left: 10px;
-		font-family: 'JetBrains Mono';
-		color: var(--text-color);
-		margin-right: 50vw;
-		min-width: 300px;
-		font-size: 3vh;
-	}
-	.endpara {
-		margin-bottom: 3vh;
-	}
-	#header{
-		position: fixed;
-		width: 100vw;
-	}
-	.highlight {
-		color: none;
-		background: linear-gradient(90deg, rgba(18,53,135,1) 0%, rgba(24,180,177,1) 75%);
-		-webkit-background-clip: text;
-    	-webkit-text-fill-color: transparent;
-	}
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --text-color: #d0d0d0;
+    }
+  }
+  @media (prefers-color-scheme: light) {
+    :root {
+      --nav-background-color: #ffffff;
+      --text-color: #2e2e2e;
+    }
+  }
+  h1 {
+    font-family: "JetBrains Mono";
+    padding-top: 15px;
+    padding-left: 10px;
+    color: var(--text-color);
+    font-size: 7vh;
+  }
+  p {
+    padding-left: 10px;
+    font-family: "JetBrains Mono";
+    color: var(--text-color);
+    margin-right: 50vw;
+    min-width: 300px;
+    font-size: 3vh;
+  }
+  .endpara {
+    margin-bottom: 3vh;
+  }
+  #header {
+    position: fixed;
+    width: 100vw;
+  }
+  .highlight {
+    color: none;
+    background: linear-gradient(
+      90deg,
+      rgba(18, 53, 135, 1) 0%,
+      rgba(24, 180, 177, 1) 75%
+    );
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
 </style>
